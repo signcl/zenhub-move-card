ccf058268bacbeefd6fb398772f094c9b5353c42
xushanchuan <aisensiy@163.com>
2021-07-21 14:54:13 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const { default: axios } = require("axios");
+const core = require("@actions/core");
+
+async function moveCardToPipeline(
+  repoId,
+  workspaceId,
+  issueId,
+  targetPipelineId
+) {
+  const url = `https://api.zenhub.com/p2/workspaces/${workspaceId}/repositories/${repoId}/issues/${issueId}/moves`;
+  const response = await axios.post(url, {
+    pipeline_id: targetPipelineId,
+    position: "top",
+  });
+  console.log(`POST ${url} -- [${response.status}]`);
+}
+
+async function getIdOfPipelineByName(repoId, workspaceId, pipelineName) {
+  const url = `https://api.zenhub.com/p2/workspaces/${workspaceId}/repositories/${repoId}/board`;
+  const response = await axios.get(url);
+  console.log(`GET ${url} -- [${response.status}]`);
+  const pipelines = response.data.pipelines;
+  const pipeline = pipelines.find(
+    (pipeline) => pipeline.name.indexOf(pipelineName) !== -1
+  );
+  if (pipeline) {
+    return pipeline.id;
+  } else {
+    return null;
+  }
+}
+
+function extractIssueFromPattern(message) {
+  let res = [
+    /#(?<number>\d+)/i,
+    /(?<owner>\w+)\/(?<name>issues)#(?<number>\d+)/i,
+    /https?:\/\/github.com\/(?<owner>\w+)\/(?<name>\w+)\/issues\/(?<number>\d+)/i,
+  ];
+
+  for (const re of res) {
+    const match = re.exec(message);
+    if (match) {
+      return {
+        number: match.groups.number,
+      };
+    }
+  }
+}
+
+(async function () {
+  const inputs = {
+    zhToken: core.getInput("zh-token"),
+    zhWorkspaceId: core.getInput("zh-workspace-id"),
+    zhRepoId: core.getInput("zh-repository-id"),
+    commitMessage: core.getInput("commit-message"),
+    pipelineId: core.getInput("zh-target-pipeline-id"),
+    pipelineName: core.getInput("zh-target-pipeline-name"),
+  };
+  core.debug(`Inputs: ${inputs}`);
+  if (!inputs.pipelineId && !inputs.pipelineName) {
+    core.setFailed(
+      "one of zh-target-pipeline-id and zh-target-pipeline-name is required"
+    );
+    return;
+  }
+  let issueNumber = extractIssueFromPattern(inputs.commitMessage);
+  if (!issueNumber) {
+    core.info("Failed to extract issue number, action skipped");
+    return;
+  }
+  axios.defaults.headers.common["X-Authentication-Token"] = inputs.zhToken;
+  let pipelineId;
+  if (!inputs.pipelineId && inputs.pipelineName) {
+    pipelineId = await getIdOfPipelineByName(
+      inputs.zhRepoId,
+      inputs.zhWorkspaceId,
+      inputs.pipelineName
+    );
+    if (!pipelineId) {
+      core.setFailed("No pipeline name of " + pipelineName + " found");
+      return;
+    }
+  } else {
+    pipelineId = inputs.pipelineId;
+  }
+
+  await moveCardToPipeline(
+    inputs.zhRepoId,
+    inputs.zhWorkspaceId,
+    issueNumber,
+    pipelineId
+  );
+})();
